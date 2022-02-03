87d635643e0405f30626a0fd4cdd39dc778654e8
Benjamin Semah <benjaminsemah@gmail.com>
2022-02-03 23:09:25 +0000

    Fix errors spotted by linters

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// This script below is meant as a placeholder, 
+// This script below is meant as a placeholder,
 // Delet it and use your own script here.
 
 import _ from 'lodash';
@@ -8,14 +8,14 @@ import Logo from './assets/webpack-logo.png';
 function component() {
   const element = document.createElement('div');
 
-  //Lodash, now imported by this script
+  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
   // Add the image to our existing div.
   const webpackLogo = new Image();
   webpackLogo.src = Logo;
-  
+
   element.appendChild(webpackLogo);
 
   return element;
